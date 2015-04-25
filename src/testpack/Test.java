2aa5b8331c06735819c1fb29df63163f795e4faf
Simon <simon.bjonsson@gmail.com>
2015-04-25 16:31:27 +0200

    Initial commit

---
+++ src/testpack/Test.java
@@ -0,0 +1,15 @@
+package testpack;
+import javax.swing.*;
+/**
+ * Created by Simon on 25.04.2015.
+ */
+public class Test {
+    public Test()
+    {
+        JOptionPane.showMessageDialog(null,"Dette er en test");
+    }
+    public static void main(String []args)
+    {
+        Test test = new Test();
+    }
+}
